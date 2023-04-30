c9c2b428616cddfc9fca65a40e02b3e10d5ef467
Lachlan <lachlanterrey@yahoo.com>
2023-04-30 15:42:27 +1000

    feat: add useDebounce and search bar

--- src/components/newPlan/index.tsx
+++ src/components/newPlan/index.tsx
@@ -1,12 +1,70 @@
-import { FC } from "react";
-import AddNewPlanButton from "./addNewPlanButton";
+import { useEffect, useState, FC } from "react";
+import { FoodItem } from "@/lib/types";
+import { supabase } from "@/lib/supabase";
+import { MagnifyingGlassIcon, PlusCircleIcon } from "@heroicons/react/20/solid";
+import useDebounce from "@/lib/hooks/useDebounce";
 
-const NewPlan: FC = () => {
+const SearchFood: FC = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState<FoodItem[]>([]);
+  const debouncedSearchTerm: string = useDebounce<string>(searchTerm, 200);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const search = async () => {
+    if (!debouncedSearchTerm) {
+      setSearchResults([]);
+      return;
+    }
+
+    const { data, error } = await supabase
+      .from("food")
+      .select("*")
+      .ilike("name", `%${debouncedSearchTerm}%`);
+
+    if (error) {
+      console.error("Error searching:", error);
+      return;
+    }
+
+    setSearchResults(data || []);
+  };
+
+  useEffect(() => {
+    search();
+  }, [debouncedSearchTerm]);
+
   return (
-    <div>
-      <AddNewPlanButton />
+    <div className="border border-gray-300 flex bg-white rounded-lg flex-col">
+      <div className="flex">
+        <input
+          type="text"
+          placeholder="Search food..."
+          value={searchTerm}
+          onChange={handleChange}
+          className="border-none rounded-l-lg focus:outline-none border-transparent focus:border-transparent focus:ring-0 flex-grow"
+        />
+        <button onClick={search} className="px-2">
+          <MagnifyingGlassIcon className="text-gray-500 w-6" />
+        </button>
+      </div>
+
+      {searchResults.length > 0 && (
+        <div>
+          {searchResults.map((food) => (
+            <div key={food.id} className="flex">
+              <p className="my-auto ml-2">{food.name}</p>
+              <button className="ml-auto mr-2 my-2">
+                <PlusCircleIcon className="w-6" />
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
 
-export default NewPlan;
+export default SearchFood;
